dc55904840689c8cc6916b87dee02c4d15037146
Pablo Jose <pablojjt06@gmail.com>
2025-08-12 14:36:25 -0500

    Se agrega la iteracion para que aparezcan los nombres en pantalla

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ function agregarAmigo() {
 function mostrarAmigos() {
     let lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
 }
 
    
